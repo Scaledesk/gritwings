3b1406e5e48c7b59a9eba4bdb6c2b35f5ccbe338
Kartik <kartikagarwal.1993@gmail.com>
2015-12-31 03:07:27 +0530

    c

--- app/User.php
+++ app/User.php
@@ -7,11 +7,11 @@ use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-
+use Cmgmyr\Messenger\Traits\Messagable;
 
 class User extends Model implements AuthenticatableContract,AuthorizableContract,CanResetPasswordContract
 {
-    use Authenticatable, Authorizable, CanResetPassword;
+    use Authenticatable, Authorizable, CanResetPassword,  Messagable;
     const ID                                = 'id';
     const NAME                              = 'name';
     const EMAIL                             = 'email';
